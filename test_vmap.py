ad263b397a38cb5ff75f9b3350846b41bb34440a
Sorina Lupu <lupusorina@yahoo.com>
2025-02-21 10:49:27 -0800

    vmap benchmarking

--- test_vmap.py
+++ test_vmap.py
@@ -5,26 +5,36 @@ from jax import vmap
 import jax.numpy as jnp
 import time
 
-# Define a function to add two numbers
 def add(x, y):
     return x + y
 
-time_1 = time.time()
-x_vector = jnp.arange(10000000)
-y_vector = jnp.arange(10000000)
-add_vectorized = vmap(add)
-print("Time for vmap:", time.time() - time_1)
+times = []
+N = 18
 
-# Compute element-wise addition
-time_2 = time.time()
-result_vectorized = add_vectorized(x_vector, y_vector)
-time_3 = time.time()
-print("JAX Summation Time taken:{} seconds".format(time_3 - time_2))
-# print("Vectorized Addition:", result_vectorized)
+for i in range(0, N):
+    print("Power of 10:", i)
+    x_vector = jnp.arange(10**i)
+    y_vector = jnp.arange(10**i)
+    add_vectorized = vmap(add)
 
-# do the same with numpy
+    # Compute element-wise addition
+    time_2 = time.time()
+    result_vectorized = add_vectorized(x_vector, y_vector)
+    times.append(time.time() - time_2)
+
+# Plot
+import matplotlib.pyplot as plt
+plt.plot(range(0, N), times, 'o-')
+plt.xlabel("Power of 10")
+plt.ylabel("Time taken in seconds")
+plt.title("Time taken for vmap")
+plt.xticks(range(0, N))
+plt.grid()
+plt.savefig("vmap_time.png")
+plt.show()
+
+# Check the time taken for numpy
 import numpy as np
-
 x_np = np.arange(10000000)
 y_np = np.arange(10000000)
 time_4 = time.time()
