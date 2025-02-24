80a24b1f316079ea07594e1a22e52927294de86e
Sorina <lupusorina@yahoo.com>
2025-02-23 21:20:53 -0800

    update from 2 computers

--- data/data_processing.py
+++ data/data_processing.py
@@ -4,30 +4,47 @@ iter = [1, 2, 3, 4, 5]
 
 df_cpu_list = []
 df_gpu_list = []
+df_gpu_fremont_list = []
+df_cpu_fremont_list = []
 for i in iter:
     df = pd.read_csv("vmap_time_macbook_jax_cpu_" + str(i) + ".csv")
     df_cpu_list.append(df)
     df = pd.read_csv("vmap_time_macbook_jax_gpu_" + str(i) + ".csv")
     df_gpu_list.append(df)
+    df = pd.read_csv("vmap_time_fremont_jax_gpu_" + str(i) + ".csv")
+    df_gpu_fremont_list.append(df)
+    df = pd.read_csv("vmap_time_fremont_jax_cpu_" + str(i) + ".csv")
+    df_cpu_fremont_list.append(df)
     
 df_cpu = pd.concat(df_cpu_list)
 df_gpu = pd.concat(df_gpu_list)
+df_gpu_fremont = pd.concat(df_gpu_fremont_list)
+df_cpu_fremont = pd.concat(df_cpu_fremont_list)
 
 mean_cpu = df_cpu.groupby("Power").mean()
 std_cpu = df_cpu.groupby("Power").std()
 mean_gpu = df_gpu.groupby("Power").mean()
 std_gpu = df_gpu.groupby("Power").std()
+mean_gpu_fremont = df_gpu_fremont.groupby("Power").mean()
+std_gpu_fremont = df_gpu_fremont.groupby("Power").std()
+mean_cpu_fremont = df_cpu_fremont.groupby("Power").mean()
+std_cpu_fremont = df_cpu_fremont.groupby("Power").std()
 
 # Plot.
 import matplotlib.pyplot as plt
 plt.plot(mean_cpu.index, mean_cpu["Time"], 'o-', label="CPU Apple M1")
 plt.fill_between(mean_cpu.index, mean_cpu["Time"] - std_cpu["Time"], mean_cpu["Time"] + std_cpu["Time"], alpha=0.3)
+plt.plot(mean_cpu_fremont.index, mean_cpu_fremont["Time"], 'o-', label="CPU Intel i9")
+plt.fill_between(mean_cpu_fremont.index, mean_cpu_fremont["Time"] - std_cpu_fremont["Time"], mean_cpu_fremont["Time"] + std_cpu_fremont["Time"], alpha=0.3)
 plt.plot(mean_gpu.index, mean_gpu["Time"], 'o-', label="GPU Apple M1")
 plt.fill_between(mean_gpu.index, mean_gpu["Time"] - std_gpu["Time"], mean_gpu["Time"] + std_gpu["Time"], alpha=0.3)
+plt.plot(mean_gpu_fremont.index, mean_gpu_fremont["Time"], 'o-', label="GPU NVIDIA TITAN RTX")
+plt.fill_between(mean_gpu_fremont.index, mean_gpu_fremont["Time"] - std_gpu_fremont["Time"], mean_gpu_fremont["Time"] + std_gpu_fremont["Time"], alpha=0.3)
 plt.xlabel("Power of 10")
 plt.ylabel("Time taken in seconds")
 plt.title("Time taken for vmap")
 plt.xticks(mean_cpu.index)
 plt.grid()
 plt.legend()
-plt.savefig("vmap_time_macbook.png")
+plt.show()
+plt.savefig("vmap_time_macbook_rtx.png")
--- readme.md
+++ readme.md
@@ -3,6 +3,6 @@ Some simple examples to learn the functionality of JAX and just-in-time (JIT).
 This is useful for MuJoCo-MJX https://mujoco.readthedocs.io/en/stable/mjx.html
 
 
-### Performance Apple M1 
+### Performance Apple M1 vs NVIDIA TITAN RTX
 
-![Performance Apple M1](data/vmap_time_macbook.png)
+![Performance Apple M1](data/vmap_time_macbook_rtx.png)
